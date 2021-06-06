49b04e369acab357e6e6152ff24d2bfb5f273334
s-hann <hanna22n@gmail.com>
2021-06-06 16:47:04 +0700

    activity_home.xml

---
+++ app/src/main/java/com/example/kuuf_project/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.kuuf_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class HomeActivity extends AppCompatActivity {
+    ListView lvTransHistory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
